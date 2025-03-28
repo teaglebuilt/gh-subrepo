1584307eb5005001efa56ae735acb467628603d2
teaglebuilt <dillan.teagle.va@gmail.com>
2025-03-28 10:30:52 -0400

    fix status package

--- cmd/status/status_test.go
+++ cmd/status/status_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	utils "github.com/teaglebuilt/gh-subrepo/internal"
 )
 
 func TestFindAllSubrepos(t *testing.T) {
@@ -44,42 +45,42 @@ func TestStatusCmd_Integration(t *testing.T) {
 	assert.NoError(t, err)
 	defer os.RemoveAll(rootDir)
 
-	assert.NoError(t, ExecCmd(rootDir, "git", "init"))
+	assert.NoError(t, utils.ExecCmd(rootDir, "git", "init"))
 
 	remoteRepo, err := os.MkdirTemp("", "remote-subrepo-*")
 	assert.NoError(t, err)
 	defer os.RemoveAll(remoteRepo)
 
-	assert.NoError(t, ExecCmd(remoteRepo, "git", "init", "--bare"))
+	assert.NoError(t, utils.ExecCmd(remoteRepo, "git", "init", "--bare"))
 
 	tmpWork, err := os.MkdirTemp("", "remote-workspace-*")
 	assert.NoError(t, err)
 	defer os.RemoveAll(tmpWork)
 
-	assert.NoError(t, ExecCmd("", "git", "clone", remoteRepo, tmpWork))
+	assert.NoError(t, utils.ExecCmd("", "git", "clone", remoteRepo, tmpWork))
 
 	os.WriteFile(filepath.Join(tmpWork, "README.md"), []byte("Initial remote content"), 0644)
-	assert.NoError(t, ExecCmd(tmpWork, "git", "add", "."))
-	assert.NoError(t, ExecCmd(tmpWork, "git", "commit", "-m", "Initial commit"))
-	assert.NoError(t, ExecCmd(tmpWork, "git", "push", "origin", "master"))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "add", "."))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "commit", "-m", "Initial commit"))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "push", "origin", "master"))
 
 	subrepoDir := "vendor/lib"
-	assert.NoError(t, ExecCmd(rootDir, "git", "clone", remoteRepo, filepath.Join(rootDir, subrepoDir)))
+	assert.NoError(t, utils.ExecCmd(rootDir, "git", "clone", remoteRepo, filepath.Join(rootDir, subrepoDir)))
 
 	os.RemoveAll(filepath.Join(rootDir, subrepoDir, ".git"))
 
 	gitrepoContent := "[subrepo]\nremote = " + remoteRepo + "\nbranch = master\n"
 	os.WriteFile(filepath.Join(rootDir, subrepoDir, ".gitrepo"), []byte(gitrepoContent), 0644)
 
-	assert.NoError(t, ExecCmd(rootDir, "git", "add", "."))
-	assert.NoError(t, ExecCmd(rootDir, "git", "commit", "-m", "Add subrepo"))
+	assert.NoError(t, utils.ExecCmd(rootDir, "git", "add", "."))
+	assert.NoError(t, utils.ExecCmd(rootDir, "git", "commit", "-m", "Add subrepo"))
 
 	checkStatus(rootDir, filepath.Join(rootDir, subrepoDir))
 
 	os.WriteFile(filepath.Join(tmpWork, "CHANGELOG.md"), []byte("Changelog added"), 0644)
-	assert.NoError(t, ExecCmd(tmpWork, "git", "add", "."))
-	assert.NoError(t, ExecCmd(tmpWork, "git", "commit", "-m", "Add CHANGELOG"))
-	assert.NoError(t, ExecCmd(tmpWork, "git", "push", "origin", "master"))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "add", "."))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "commit", "-m", "Add CHANGELOG"))
+	assert.NoError(t, utils.ExecCmd(tmpWork, "git", "push", "origin", "master"))
 
 	checkStatus(rootDir, filepath.Join(rootDir, subrepoDir))
 }
